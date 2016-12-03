9fa7c1f88718950916865e5f729e9a0b3ee63a43
Celena T <chibiynm@gmail.com>
2016-12-03 14:21:21 -0500

    Added about

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { EventsComponent } from './events/events.component';
@@ -9,18 +10,26 @@ import { EventsComponent } from './events/events.component';
 import { NewLinePipe } from './shared/new-line.pipe';
 
 import { DropdownModule } from 'ng2-bootstrap/components/dropdown';
+import { AboutComponent } from './about/about.component';
+
+const appRoutes: Routes = [
+  { path: '', component: EventsComponent },
+  { path: 'about', component: AboutComponent },
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     EventsComponent,
-    NewLinePipe
+    NewLinePipe,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    DropdownModule
+    DropdownModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
